9219bef2620c0b3163a71e6eca09253e1652b1dd
Alberto Cangialosi <billyballo.res@gmail.com>
2024-01-27 12:45:50 +0100

    feat: kickstart of this react exercise from prof's notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export function App() {
+  const cachedEmail = localStorage.getItem("email");
+  const cachedUsers = !!localStorage.getItem("users")
+    ? JSON.parse(localStorage.getItem("users"))
+    : {};
+  const [isLogged, setIsLogged] = useState(!!cachedEmail);
+  const [inputEmail, setInputEmail] = useState("");
+  const [users, setUsers] = useState(cachedUsers);
+
+  function onMap(user) {
+    if (user.email === inputEmail) {
+      return {
+        email: user.email,
+        counter: user.counter + 1,
+        lastAccess: new Date().toISOString(),
+      };
+    } else return user;
+  }
+
+  function onClickLogin() {
+    setIsLogged(true);
+    localStorage.setItem("email", inputEmail);
+    if (!!users[inputEmail]) {
+      const newUsers = {
+        ...users,
+        [inputEmail]: {
+          counter: users[inputEmail].counter + 1,
+          lastAccess: new Date().toISOString(),
+        },
+      };
+      setUsers(newUsers);
+      localStorage.setItem("users", JSON.stringify(newUsers));
+    } else {
+      const newUsers = {
+        ...users,
+        [inputEmail]: {
+          ...users[inputEmail],
+          lastAccess: new Date().toISOString(),
+          counter: 1,
+        },
+      };
+
+      setUsers(newUsers);
+      localStorage.setItem("users", JSON.stringify(newUsers));
+    }
+  }
+
+  function onClickLogout() {
+    setInputEmail("");
+    setIsLogged(false);
+    localStorage.removeItem("email");
+  }
+
+  function onChangeEmail(event) {
+    setInputEmail(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      {isLogged ? (
+        <>
+          {users[inputEmail].counter > 1 ? (
+            <>
+              <div>Bentornato</div>
+              <div>Ultimo accesso {users[inputEmail].lastAccess}</div>
+            </>
+          ) : (
+            <div>Benvenuto</div>
+          )}
+          <button onClick={onClickLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder="Inserisci email"
+            value={inputEmail}
+            onChange={onChangeEmail}
+          />
+          <button onClick={onClickLogin} disabled={!inputEmail}>
+            Login
+          </button>
+        </>
+      )}
+    </section>
   );
 }
+
 
 export default App;
